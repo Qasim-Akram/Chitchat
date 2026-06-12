c3fa18e396f80ea70c4ac685ec82137e216e5442
Muhammad Qasim Akram <m.qasimakram46@gmail.com>
2026-06-12 19:37:20 +0500

    tryinng to fix websocket problem in production

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,13 +1,15 @@
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+import django
+django.setup()
+
 from django.core.asgi import get_asgi_application
-
-django_asgi_app = get_asgi_application()
-
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.middleware import JwtAuthMiddleware
 import chat.routing
+
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -140,3 +140,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'auth.User'
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
